279065bb9690dc9b94772a627a1cd319ae05121d
alvinbank1 <61926776+alvinbank1@users.noreply.github.com>
2021-12-19 16:58:23 +0900

    Update indexmain.js

--- public/js/indexmain.js
+++ public/js/indexmain.js
@@ -2,7 +2,7 @@ function printName()  {
   const name = document.getElementById('username').value;
   if (name == "System")
   {
-    print("System is blocked username.")
+    alert("System is blocked username.")
     document.getElementById("username").value = '';
   }
 }
