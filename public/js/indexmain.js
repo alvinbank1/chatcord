f9192bfa9eb863fdfb0042ca2d29acb666c90e8c
alvinbank1 <61926776+alvinbank1@users.noreply.github.com>
2021-12-19 16:59:35 +0900

    Update indexmain.js

--- public/js/indexmain.js
+++ public/js/indexmain.js
@@ -5,4 +5,24 @@ function printName()  {
     alert("System is blocked username.")
     document.getElementById("username").value = '';
   }
+  if (name == "Fuck")
+  {
+    alert("Fuck is blocked username.")
+    document.getElementById("username").value = '';
+  }
+  if (name == "fuck")
+  {
+    alert("Fuck is blocked username.")
+    document.getElementById("username").value = '';
+  }
+  if (name == "shit")
+  {
+    alert("shit is blocked username.")
+    document.getElementById("username").value = '';
+  }
+  if (name == "Shit")
+  {
+    alert("shit is blocked username.")
+    document.getElementById("username").value = '';
+  }
 }
